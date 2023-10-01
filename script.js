7fccfb8f473259baafed3fa2d0a127401a55b848
kmanishankar <217r1a66f8@cmrtc.ac.in>
2023-10-01 13:10:38 +0530

    adding using json

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function showAlert(){
-    alert(" button clicked")
+
+async function getData(url)
+{
+    try{
+        let data=await fetch(url);
+
+        let s="";
+
+        data = await data.json();
+
+        for(let i=0;i<data.products.length;i++){
+
+            s = s + `<tr> <td> ${data.products[i].brand} </td>  <td>${data.products[i].category} </td> <td>${data.products[i].description}</td> </tr>`;
+
+        }
+
+        document.getElementById("schlor").innerHTML = s;
+
+        console.log(data);
+
+    }catch(error){
+       console.log(error);
+    }
 }
+
+getData("https://dummyjson.com/products")
